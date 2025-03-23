6d535bbb462b7f4e91852e0a15260dff5a7d26de
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-03-23 16:54:40 +0300

    Öncesinde Angular güncellemesi için commit

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,6 +1,10 @@
 export class UserModel{
     id: string = "";
     name: string = "";
+    firstName: string = "";
+    lastName: string = "";
+    fullName: string = "";
+    password: string = "";
     userName: string = "";
     email: string = "";
 }
