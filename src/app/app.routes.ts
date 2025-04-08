d89322b1a7b600acc5a09b010c410fb12c2126a8
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-04-08 16:40:06 +0300

    Stok karlılık raporu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,6 +16,7 @@ import { CustomerDetailsComponent } from './components/customer-details/customer
 import { ProductsComponent } from './components/products/products.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { InvoicesComponent } from './components/invoices/invoices.component';
+import { ProductProfitabilityReportsComponent } from './components/product-profitability-reports/product-profitability-reports.component';
 
 export const routes: Routes = [
     {
@@ -102,6 +103,15 @@ export const routes: Routes = [
             {
                 path: "invoices",
                 component: InvoicesComponent
+            },
+            {
+                path: "reports",
+                children:[
+                    {
+                        path: "product-profitability-reports",
+                        component: ProductProfitabilityReportsComponent
+                    }
+                ]
             }
         ]
     }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../modules/shared.module';
+declare const Chart:any;
 
 @Component({
     selector: 'app-home',
@@ -8,4 +9,27 @@ import { SharedModule } from '../../modules/shared.module';
     styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+showChart(){
+const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+    }
 }
